378bb0870472bbd13643864612c67c28dfc9aada
Slival <Guillaumelm@msn.com>
2018-10-18 20:37:56 -0400

    Creation of Rooms, Kromrak, PlayerMovement script and Room Code.

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Salle currentRoom;
+
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Krom existe!");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyUp("w"))
+        {
+            currentRoom = currentRoom.north;
+            Debug.Log("WwwWwWWWWWWWWWWWWW");
+        }
+        if (Input.GetKeyUp("a"))
+        {
+            currentRoom = currentRoom.west;
+            Debug.Log("Aaa!");
+        }
+        if (Input.GetKeyUp("s"))
+        {
+            currentRoom = currentRoom.south;
+            Debug.Log("Sss!");
+        }
+        if (Input.GetKeyUp("d"))
+        {
+            currentRoom = currentRoom.east;
+            Debug.Log("Ddd!");
+        }
+    }
+}
---
+++ Assets/Salle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Salle : MonoBehaviour {
+
+    public Salle north;
+    public Salle south;
+    public Salle east;
+    public Salle west;
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log("Bravo, une salle existe!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+}
